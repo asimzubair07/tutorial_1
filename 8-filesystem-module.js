d7e6f57fea80703ff1db52c0327f8515f1cce557
Syed Asim Zubair <50781637+asimzubair07@users.noreply.github.com>
2022-10-25 12:21:19 +0500

    read file sync

---
+++ 8-filesystem-module.js
@@ -0,0 +1,14 @@
+const {
+    readFileSync,
+    writeFileSync
+} = require(`fs`)
+
+const first = readFileSync(`./content/first.txt`)
+const second = readFileSync(`./content/second.txt`)
+
+// ye ya to file create kr dega warna existing file main write krdega
+writeFileSync(`./content/result-sync.txt`, `Here is the result: ${first},${second}`)
+// flag file main append kary ga
+writeFileSync(`./content/result-sync.txt`, `Here is the result: ${first},${second}`, {
+    flag: 'a'
+})
